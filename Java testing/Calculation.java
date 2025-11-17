6995ad2f7a103b51d1cbb2a59d75dc7f691f4553
ajaykumar-collab <ajaykumarfor007@gmail.com>
2025-11-17 13:44:39 +0530

    Add sout in branch

--- Java testing/Calculation.java
+++ Java testing/Calculation.java
@@ -4,6 +4,7 @@ public class Calculation {
         for (int i = 1; i <= n; i++) {
             sum += i;
         }
+        System.out.println( "Sum of 1 to " + n + " is " + sum);
         return sum;
     }
     public static int product(int n) {
@@ -11,6 +12,7 @@ public class Calculation {
         for (int i = 1; i <= n; i++) {
             product *= i;
         }
+        System.out.println( "Product of 1 to " + n + " is " + product);
         return product;
     }
 }
