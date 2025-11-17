3842990fd534611cf1992e7e17fde075f95fc0a1
ajaykumar-collab <ajaykumarfor007@gmail.com>
2025-11-17 12:28:56 +0530

    Added function

--- Java testing/Test.java
+++ Java testing/Test.java
@@ -2,6 +2,12 @@ public class Test
 {
     public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+        int sum=0;
+        for(int i=1;i<=100;i++)
+        {
+            sum+=i;
+        }
+        System.out.println("Sum of 1 to 100 is "+sum);
+
 	}
 }
