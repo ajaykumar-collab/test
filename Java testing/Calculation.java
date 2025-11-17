a4fc7f6f9b79d94eab63c7cffafaa9406ecf2e06
ajaykumar-collab <ajaykumarfor007@gmail.com>
2025-11-17 13:43:47 +0530

    Add sout

--- Java testing/Calculation.java
+++ Java testing/Calculation.java
@@ -4,6 +4,7 @@ public class Calculation {
         for (int i = 1; i <= n; i++) {
             sum += i;
         }
+        System.out.println(sum);
         return sum;
     }
     public static int product(int n) {
@@ -11,6 +12,7 @@ public class Calculation {
         for (int i = 1; i <= n; i++) {
             product *= i;
         }
+        System.out.println(product);
         return product;
     }
 }
